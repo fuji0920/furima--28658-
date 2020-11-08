298435e9b0c6034da968bb1ea2999cc01df4e5ac
fuji0808 <69977636+fuji0808@users.noreply.github.com>
2020-11-08 18:30:08 +0900

    エラーメッセージ表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :password, presence: true, format: { with: /\A[0-9]+\z/ && /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+  validates :nickname, presence: true  
+  validates :password, format: { with: /\A[0-9]+\z/ && /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
   validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
   validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
