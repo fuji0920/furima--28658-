9835dafcb8939bea5f0215108ebf0d942dec3a40
fuji0808 <69977636+fuji0808@users.noreply.github.com>
2020-11-09 21:43:36 +0900

    ユーザーモデルの単体テストコード作成

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -8,6 +8,6 @@ FactoryBot.define do
     first_name            {"陸太郎"}
     last_name_kana        {"ヤマダ"}
     first_name_kana       {"リクタロウ"}
-    birthday              {Faker::Date.between_except(from: '1930-01-01', to: '2015-12-31'}
+    birthday              {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
   end
 end
