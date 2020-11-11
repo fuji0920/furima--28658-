a6cb309cf3b1fa8cae0abe8481b4b9d00251f193
fuji0808 <69977636+fuji0808@users.noreply.github.com>
2020-11-11 20:38:31 +0900

    ユーザーモデル作成５

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Internet.username}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
+    password              {"hoge1234"}
     password_confirmation {password}
     last_name             {"山田"}
     first_name            {"陸太郎"}
