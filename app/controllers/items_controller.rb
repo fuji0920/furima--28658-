45580a33415c1cd69dc96bdbfbc51c6e7ade48e2
fuji0808 <69977636+fuji0808@users.noreply.github.com>
2020-11-23 21:00:21 +0900

    ログインユーザー兼出品者が削除ができる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,10 +37,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.destroy
-        redirect_to root_path
-    else
-        render 'edit'
+    if current_user.id == @item.user_id
+        @item.destroy
+         redirect_to root_path
     end
   end
 
