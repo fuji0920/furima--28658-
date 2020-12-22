b940e89495710947c2054118c8254bdca83a4b66
fuji0808 <69977636+fuji0808@users.noreply.github.com>
2020-12-22 15:22:11 +0900

    revert routes.rbの更新

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,4 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
-<<<<<<< HEAD
-<<<<<<< HEAD
-    resources :orders
-=======
->>>>>>> parent of 9b37c7a... orderコントローラー作成
-=======
-  resources :orders
->>>>>>> parent of 9213730... order controller create action編集
 end
