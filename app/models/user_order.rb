22234acdd9acb0e4d0f5ce5d064f4d9c53d4a68a
fuji0808 <69977636+fuji0808@users.noreply.github.com>
2020-12-22 15:29:15 +0900

    商品購入1から移動

---
+++ app/models/user_order.rb
@@ -0,0 +1,7 @@
+class クラス名
+  include ActiveModel::Model
+  attr_accessor :post, :prefecture_id, :city, :address, :bilding, :tel
+
+  def save
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+    resources :orders
+>>>>>>> parent of f2c3718... revert orderコントローラー作成
+=======
+    resources :orders
+=======
+>>>>>>> parent of 9b37c7a... orderコントローラー作成
+>>>>>>> parent of 0867277... revert order controller create action編集
 end
